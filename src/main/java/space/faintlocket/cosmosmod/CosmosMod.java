f898d8c7fb1d3698878437c3352de7a9b540cb01
BuildTools <unconfigured@null.spigotmc.org>
2023-11-10 23:47:35 +0000

    Initial Commit

---
+++ src/main/java/space/faintlocket/cosmosmod/CosmosMod.java
@@ -0,0 +1,13 @@
+package space.faintlocket.cosmosmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CosmosMod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/space/faintlocket/cosmosmod/client/CosmosModClient.java
@@ -0,0 +1,13 @@
+package space.faintlocket.cosmosmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CosmosModClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
